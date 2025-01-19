a4cdaa98e709093fe51c77621690aa97f7fa2b1e
Anmit Ray <anmitray@Anmits-Mac-mini.local>
2025-01-19 09:53:55 +0530

    Gracefully shutting down grpc server

--- grpc-todo/cmd/server/main.go
+++ grpc-todo/cmd/server/main.go
@@ -1,26 +1,55 @@
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
 	"net"
+	"os"
+	"os/signal"
 
 	"github.com/anmit007/go-grpc-todo/internal/todo"
 	"github.com/anmit007/go-grpc-todo/proto"
+	"golang.org/x/sync/errgroup"
+
 	"google.golang.org/grpc"
 )
 
 func main() {
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	if err := run(ctx); err != nil && !errors.Is(err, context.Canceled) {
+		slog.Error("error running app", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+	slog.Info("closing server gracefully")
+}
+func run(ctx context.Context) error {
 	grpcServer := grpc.NewServer()
 	todoService := todo.NewService()
 	proto.RegisterTodoServiceServer(grpcServer, todoService)
+	const addr = ":50051"
 
-	listen, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("STARTING SERVER ON ADDRESS %s ", ":50051")
-	if err := grpcServer.Serve(listen); err != nil {
-		log.Fatal(err)
-	}
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		listen, err := net.Listen("tcp", addr)
+		if err != nil {
+			return fmt.Errorf("failed to listen on address: %w", err)
+		}
+		slog.Info("STARTING SERVER ON", slog.String("ADDRESS", addr))
+		if err := grpcServer.Serve(listen); err != nil {
+			return fmt.Errorf("failed to serve grpc service :%w", err)
+		}
+		return nil
+	})
+	g.Go(func() error {
+		<-ctx.Done()
+		grpcServer.GracefulStop()
+		return nil
+	})
+	return g.Wait()
 }
--- grpc-todo/go.mod
+++ grpc-todo/go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
